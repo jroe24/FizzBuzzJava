3d89511ff21d0919df1b6e15e6f12dfb03ce5582
Jacob Roe <jroenavy24@gmail.com>
2021-06-07 15:53:32 -0700

    first commit

---
+++ FizzBuzzTest.java
@@ -0,0 +1,10 @@
+public class FizzBuzzTest {
+
+    public static void main(String[] args) {
+
+        FizzBuzz f = new FizzBuzz();
+        String answer = f.fizzBuzz(1);
+        System.out.println(answer);
+    }
+    
+}
